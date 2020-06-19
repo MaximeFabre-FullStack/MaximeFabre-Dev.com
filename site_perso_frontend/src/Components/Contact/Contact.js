fb9789ed641fe11ba44d1b64e8907ae0d8a33589
Maxime FABRE <maxusdm06@gmail.com>
2020-06-20 00:09:48 +0200

    Refactoring

--- site_perso_frontend/src/Components/Contact/Contact.js
+++ site_perso_frontend/src/Components/Contact/Contact.js
@@ -4,7 +4,7 @@ import Footer from "../Footer/Footer";
 import { Link } from "react-router-dom";
 
 import "./Contact.css";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { OverlayTrigger, Tooltip, Form } from "react-bootstrap";
 
 class Contact extends Component {
   render() {
@@ -36,7 +36,26 @@ class Contact extends Component {
           </div>
         </div>
 
-        <p>contact me</p>
+        <Form>
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Example select</Form.Label>
+            <Form.Control as="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+        </Form>
 
         <Footer />
       </div>
--- site_perso_frontend/src/ComponentsFR/ContactFr/ContactFR.js
+++ site_perso_frontend/src/ComponentsFR/ContactFr/ContactFR.js
@@ -4,9 +4,23 @@ import FooterFR from "../FooterFR/FooterFR";
 import { Link } from "react-router-dom";
 
 import "./ContactFR.css";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { OverlayTrigger, Tooltip, Form, Button } from "react-bootstrap";
 
 class ContactFR extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      option: "Un(e) client(e) potentiel(le)",
+      message: "",
+    };
+  }
+
+  handle_changeFR = async (e) => {
+    await this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state.email, this.state.option, this.state.message);
+  };
+
   render() {
     return (
       <div className="contactFR">
@@ -36,7 +50,46 @@ class ContactFR extends Component {
           </div>
         </div>
 
-        <p>Contactez moi</p>
+        <Form className="formFR">
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label>Votre adresse mail :</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="nom@exemple.com"
+              name="email"
+              onChange={this.handle_changeFR}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Vous etes :</Form.Label>
+            <Form.Control
+              as="select"
+              name="option"
+              onChange={this.handle_changeFR}
+            >
+              <option>Un(e) client(e) potentiel(le)</option>
+              <option>Un(e) recruteur(se) / Une agence de recrutement</option>
+              <option>
+                Un(e) chargé(e) de Ressources Humaines a la recherche d'un
+                developpeur
+              </option>
+              <option>
+                Rien de tout ça, mais je souhaite tout de meme rentrer en
+                contact
+              </option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Votre message : </Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="5"
+              name="message"
+              onChange={this.handle_changeFR}
+            />
+          </Form.Group>
+          <Button className="submit-button">C'est parti</Button>
+        </Form>
 
         <FooterFR />
       </div>
